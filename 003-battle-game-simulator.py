8ae90fddd0e193775d6caf5d43f3d67c699d2306
Jan Klacan <156678842+jan-klacan@users.noreply.github.com>
2025-06-01 20:28:26 +0200

    upload battle game simulator v1
    just the Character class so far

---
+++ 003-battle-game-simulator.py
@@ -0,0 +1,107 @@
+import random
+
+
+class Character():
+    baseline_health = 2
+    baseline_luck = 0.2
+    max_luck = 1.0
+
+    def __init__(self, name: str, health_extra: int, luck_extra: float):
+        self.name = name
+
+        # Setting up the starting health parameters
+        if health_extra < 0:
+            raise ValueError("Extra health cannot be negative.")
+        self.__health_extra = health_extra
+        self.__max_health = Character.baseline_health + health_extra
+        self.__current_health = self.__max_health
+
+        # Setting up the starting luck parameters
+        if luck_extra < 0 or (Character.baseline_luck + luck_extra) > Character.max_luck:
+            raise ValueError(f"Luck must be between {Character.baseline_luck} and {Character.max_luck}")
+        self.__luck_extra = luck_extra
+        self.__starting_luck = Character.baseline_luck + luck_extra
+        self.__current_luck = self.__starting_luck
+        self.__luck_total = self.__starting_luck
+
+
+    # -- Health section --
+    
+    @property
+    def health_extra(self) -> int:
+        return self.__health_extra
+    
+    @health_extra.setter
+    def health_extra(self, value: int):
+        if value < 0:
+            raise ValueError("Extra health cannot be negative.")
+        self.__health_extra = value
+        self.__max_health = Character.baseline_health + value
+
+        if self.__current_health > self.__max_health:
+            self.__current_health = self.__max_health
+    
+    @property
+    def max_health(self) -> int:
+        return self.__max_health
+    
+    @property
+    def current_health(self) -> int:
+        return self.__current_health
+    
+    @current_health.setter
+    def current_health(self, value: int):
+        if value < 0 or value > self.__max_health:
+            raise ValueError(f"Current health must be between 0 and {self.__max_health}.")
+        self.__current_health = value
+
+    def change_health(self, delta: int):
+        new_health = self.__current_health + delta
+        if new_health < 0:
+            new_health = 0
+        elif new_health > self.__max_health:
+            new_health = self.__max_health
+        self.__current_health = new_health
+
+    # (Check if character is alive)
+    @property
+    def is_alive(self) -> bool:
+        return self.__current_health > 0
+    
+
+    # -- Luck section --
+    
+    @property
+    def luck_extra(self) -> float:
+        return self.__luck_extra
+    
+    @luck_extra.setter
+    def luck_extra(self, value: float):
+        if value < 0 or (Character.baseline_luck + value) > Character.max_luck:
+            raise ValueError(f"Extra luck must be between 0 and {Character.max_luck - Character.baseline_luck}.")
+        self.__luck_extra = value
+        self.__luck_total = Character.baseline_luck + value
+        if self.__current_luck > self.__luck_total:
+            self.__current_luck = self.__luck_total
+
+    @property
+    def luck_total(self) -> float:
+        return self.__luck_total
+
+    @property
+    def current_luck(self) -> float:
+        return self.__current_luck
+    
+    @current_luck.setter
+    def current_luck(self, value: float):
+        if value < 0 or value > self.__luck_total:
+            raise ValueError(f"Current luck cannot be negative and higher than {self.__luck_total}.")
+        self.__current_luck = value
+    
+    def change_luck(self, delta: float):
+        new_luck = self.__current_luck + delta
+        if new_luck < 0:
+            new_luck = 0
+        elif new_luck > self.__luck_total:
+            new_luck = self.__luck_total
+        self.__current_luck = new_luck
